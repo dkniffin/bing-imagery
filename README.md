307eded04a9d5c713cd6b156be512543d9dae4f0
Richard Green <vanchagreen@gmail.com>
2015-02-04 18:34:21 -0700

    Added NSEW example

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Usage
 - `npm install` - installs dependencies
 - `node bin/www` - starts the application
 - go to http://localhost:3000
+
+Dev
+====
+- Run `watchify public/javascripts/app.js -o public/javascripts/bundle.js -d` to set up a watch for browserify
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,8 @@
 Map = require('./map.js');
 
 var map = new Map(document.getElementById("myMap"), new Microsoft.Maps.Location(40, -105.27), 15);
+
+document.getElementById("start").onclick = function() {
+  //send stuff to the backend
+  alert(JSON.stringify(map.getNSEW()));
+}
